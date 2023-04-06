4eed49aeeac12fbdaf2c428cc4e2726194ff337e
Maycon Jardim <mayconsmill@gmail.com>
2023-04-05 21:06:51 -0300

    UserController criado.

---
+++ TaskSystem/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TaskSystem.Models;
+
+namespace TaskSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        [HttpGet]
+        public ActionResult<List<UserModel>> findAllUsers()
+        {
+            return Ok();
+        }
+
+    }
+}
