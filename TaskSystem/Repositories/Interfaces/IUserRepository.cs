3700e74bbc277c157b2c6f871078d058b9aa1cbb
Maycon Jardim <mayconsmill@gmail.com>
2023-04-05 21:21:21 -0300

    Criando Inteface de Usuario Repository.

---
+++ TaskSystem/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,19 @@
+﻿using TaskSystem.Models;
+
+namespace TaskSystem.Repositories.Interfaces
+{
+    public interface IUserRepository
+    {
+
+        Task<List<UserModel>> FindAllUsers();
+
+        Task<UserModel> FindById(int id);
+
+        Task<UserModel> create(UserModel user);
+
+        Task<UserModel> update(UserModel user);
+
+        Task<bool> delete(int id);
+
+    }
+}
